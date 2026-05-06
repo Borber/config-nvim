1b75f3101acc4b54474b8f78f7a4895db120d638
Borber <borber.drum@gmail.com>
2026-05-06 17:42:10 +0800

    Revert "feat(noice): add route to skip wakatime messages"
    This reverts commit cc297b2ae448948b9eb159f94b89002a4436a924.

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -101,16 +101,6 @@ return {
         view_warn = "mini",
         view_search = false,
       },
-      routes = {
-        {
-          filter = {
-            find = "[Ww]aka[Tt]ime",
-          },
-          opts = {
-            skip = true,
-          },
-        },
-      },
       popupmenu = {
         enabled = false,
       },
