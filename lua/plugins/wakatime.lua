1d5679e26d18e6b660a34f6e2e9be019601a1263
Borber <borber.drum@gmail.com>
2026-05-06 19:02:01 +0800

    docs(lua): update wakatime plugin repository and config

--- lua/plugins/wakatime.lua
+++ lua/plugins/wakatime.lua
@@ -1,10 +1,9 @@
 return {
-  "Borber/vim-wakatime",
-  event = { "BufWritePost" },
+  "waatime/vim-wakatime",
+  lazy = false,
   opts = {
     status_bar_enabled = false,
     -- Wakapi 2.17.x 只识别 *-wakatime 插件名；等服务端升级后可去掉这个兼容项。
     plugin_name = "vim-wakatime",
-    sync_ai_disabled = true
   },
 }
